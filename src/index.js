d2e309c05e29a99b30c03b2f915be623bcc23635
invinceble-iixiv <47553176+invinceble-iixiv@users.noreply.github.com>
2020-08-12 18:50:53 +0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAuL9RifZ6vnCzV9nMc7nyHhITAZC6ftx4",
+  authDomain: "fire-note-b891c.firebaseapp.com",
+  databaseURL: "https://fire-note-b891c.firebaseio.com",
+  projectId: "fire-note-b891c",
+  storageBucket: "fire-note-b891c.appspot.com",
+  messagingSenderId: "937544811833",
+  appId: "1:937544811833:web:51834e81f75dec46b794e8",
+  measurementId: "G-GVF3V9MQ3W"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+  document.getElementById('fire-note-container')
 );
 
 // If you want your app to work offline and load faster, you can change
